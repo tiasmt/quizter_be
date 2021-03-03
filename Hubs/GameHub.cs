a1278bb65b5fc2381c1f1dc81d8ab81b48692d1a
Tias Fenech <tiasfenech@gmail.com>
2021-03-03 22:06:06 +0100

    First commit with multiplayer-multigame

--- Hubs/GameHub.cs
+++ Hubs/GameHub.cs
@@ -24,6 +24,11 @@ namespace quizter_be.Hubs
             _questionService = questionService;
         }
 
+        public void JoinGroup(string gameName)
+        {
+            _hubContext.Groups.AddToGroupAsync(Context.ConnectionId, gameName); 
+        }
+
         public async Task CreateTimers(string gameName)
         {
             await CreateGameTimer(gameName);
@@ -59,7 +64,7 @@ namespace quizter_be.Hubs
                 }
 
                 var players = await _gameService.GetPlayers(gameName);
-                await _hubContext.Clients.All.SendLeaderboard(players);
+                await _hubContext.Clients.Groups(gameName).SendLeaderboard(players);
             }
         }
 
@@ -82,7 +87,7 @@ namespace quizter_be.Hubs
         public async void HeartBeat(object sender, System.Timers.ElapsedEventArgs e, string gameName)
         {
             _timeLeft--;
-            await _hubContext.Clients.All.Beat(_timeLeft);
+            await _hubContext.Clients.Groups(gameName).Beat(_timeLeft);
             if (_timeLeft <= 0)
             {
                 _timeLeft = _initialTime;
@@ -93,7 +98,7 @@ namespace quizter_be.Hubs
                     StopTimer(gameTimer);
                 }
 
-                await CheckAnswer();
+                await CheckAnswer(gameName);
                 if (_questionTimers.TryGetValue(gameName, out Timer questionTimer))
                 {
                     StartTimer(questionTimer);
@@ -107,14 +112,14 @@ namespace quizter_be.Hubs
             return game.GameSettings.TimePerQuestion + 1;
         }
 
-        private async Task CheckAnswer()
+        private async Task CheckAnswer(string gameName)
         {
-            await _hubContext.Clients.All.CheckAnswer("CheckAnswer");
+            await _hubContext.Clients.Groups(gameName).CheckAnswer("CheckAnswer");
         }
 
         private async Task SendQuestion(string gameName, Question question)
         {
-            await _hubContext.Clients.All.SendQuestion(question);
+            await _hubContext.Clients.Groups(gameName).SendQuestion(question);
             if (_questionTimers.TryGetValue(gameName, out Timer questionTimer))
             {
                 StopTimer(questionTimer);
